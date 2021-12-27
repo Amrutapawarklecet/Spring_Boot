c55e3f91a8c1e205c6acda086f9791f9441de06b
Amrutapawarklecet <96716109+Amrutapawarklecet@users.noreply.github.com>
2021-12-27 15:22:19 +0530

    Add files via upload

---
+++ BootDemo_003_sts/src/main/java/com/example/demo/BootDemo003StsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDemo003StsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDemo003StsApplication.class, args);
+	}
+
+}
